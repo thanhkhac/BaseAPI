9d561a94433674254dcf978e3dcc52a05950db1d
Khac Thanh <thanhcqb2048@gmail.com>
2025-01-06 15:59:53 +0700

    Init, add logging

---
+++ BaseAPI/Program.cs
@@ -0,0 +1,36 @@
+using BaseAPI.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+using NLog;
+
+namespace BaseAPI;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+        builder.Services.ConfigureCors();
+        builder.Services.ConfigureIISIntegration();
+        builder.Services.AddControllers();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment()) { app.UseDeveloperExceptionPage(); }
+        else { app.UseHsts(); }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseForwardedHeaders(new ForwardedHeadersOptions
+        {
+            ForwardedHeaders = ForwardedHeaders.All
+        });
+
+        app.UseCors("CorsPolicy");
+
+        app.UseAuthorization();
+        app.MapControllers();
+        app.Run();
+    }
+}
