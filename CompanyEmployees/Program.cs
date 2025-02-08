468869ab0d9c6787e0c571d28f9d155b05b35bef
Khac Thanh <thanhcqb2048@gmail.com>
2025-02-09 01:04:39 +0700

    Restrict Media Types
    If the client tries to negotiate for the media type the server doesn’t support, it should return the 406 Not Acceptable status
    code

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -20,7 +20,11 @@ public class Program
         builder.Services.ConfigureSqlContext(builder.Configuration);
         builder.Services.AddAutoMapper(typeof(Program));
         builder.Services.AddControllers(
-                config => { config.RespectBrowserAcceptHeader = true; }).AddXmlDataContractSerializerFormatters()
+                config =>
+                {
+                    config.RespectBrowserAcceptHeader = true;
+                    config.ReturnHttpNotAcceptable = true;
+                }).AddXmlDataContractSerializerFormatters()
             .AddApplicationPart(typeof(CompanyEmployees.Presentation.AssemblyReference).Assembly);
 
         var app = builder.Build();
