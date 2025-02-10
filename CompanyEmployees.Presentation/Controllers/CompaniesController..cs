9c12a23abc5be04fce15c2f78a62ff962bc25c8c
Khac Thanh <thanhcqb2048@gmail.com>
2025-02-10 22:56:29 +0700

    Add Model Binding in API

--- CompanyEmployees.Presentation/Controllers/CompaniesController..cs
+++ CompanyEmployees.Presentation/Controllers/CompaniesController..cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CompanyEmployees.Presentation.ModelBinders;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Service.Contracts;
 using Shared.DataTransferObjects;
@@ -41,7 +42,9 @@ namespace CompanyEmployees.Presentation.Controllers
         }
 
         [HttpGet("collection/({ids})", Name = "CompanyCollection")]
-        public IActionResult GetCompanyCollection(IEnumerable<Guid> ids)
+        public IActionResult GetCompanyCollection(
+            [ModelBinder(BinderType = typeof(ArrayModelBinder))]
+            IEnumerable<Guid> ids)
         {
             var companies = _service.CompanyService.GetByIds(ids, trackChanges: false);
             return Ok(companies);
@@ -51,8 +54,8 @@ namespace CompanyEmployees.Presentation.Controllers
         public IActionResult CreateCompanyCollection([FromBody] IEnumerable<CompanyForCreationDto> companyCollection)
         {
             var result = _service.CompanyService.CreateCompanyCollection(companyCollection);
-            
-            return CreatedAtRoute("CompanyCollection", new {result.ids}, result.companies);
+
+            return CreatedAtRoute("CompanyCollection", new { result.ids }, result.companies);
         }
     }
 }
