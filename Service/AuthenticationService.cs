866a11b4db672f63a92d8223a53b6d78c82c6cae
Khac Thanh <thanhcqb2048@gmail.com>
2025-02-26 11:14:07 +0700

    Add: Create User

---
+++ Service/AuthenticationService.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Contracts;
+using Entities.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Service.Contracts;
+using Shared.DataTransferObjects;
+
+namespace Service
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly ILoggerManager _logger; 
+        private readonly IMapper _mapper;
+        private readonly UserManager<User> _userManager;
+        private readonly IConfiguration _configuration;
+        public AuthenticationService(UserManager<User> userManager, ILoggerManager logger, IMapper mapper, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _logger = logger;
+            _mapper = mapper;
+            _configuration = configuration;
+        }
+
+        public async Task<IdentityResult> RegisterUser(UserForRegistrationDto userForRegistration)
+        {
+            var user = _mapper.Map<User>(userForRegistration);
+            var result = await _userManager.CreateAsync(user, userForRegistration.Password);
+            if (result.Succeeded)
+                await _userManager.AddToRolesAsync(user, userForRegistration.Roles);
+            return result;
+        }
+    }
+}
